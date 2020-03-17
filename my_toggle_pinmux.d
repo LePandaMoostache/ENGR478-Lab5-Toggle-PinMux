065572d4361fdb223ef6ed74720a85e125d5f5e3
LePandaMoostache <61212602+LePandaMoostache@users.noreply.github.com>
2020-03-17 00:26:05 -0700

    my_toggle_PinMux
    - SW2 toggles red LED
    - SW1 toggles blue LED
    Without both, toggles green LED

---
+++ my_toggle_pinmux.d
@@ -0,0 +1,12 @@
+.\my_toggle_pinmux.o: my_toggle_PinMux.c
+.\my_toggle_pinmux.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\my_toggle_pinmux.o: F:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\my_toggle_pinmux.o: my_toggle_PinMux.h
+.\my_toggle_pinmux.o: ..\..\..\..\inc/hw_types.h
+.\my_toggle_pinmux.o: ..\..\..\..\inc/hw_memmap.h
+.\my_toggle_pinmux.o: ..\..\..\..\inc/hw_gpio.h
+.\my_toggle_pinmux.o: ..\..\..\..\driverlib/sysctl.h
+.\my_toggle_pinmux.o: ..\..\..\..\driverlib/pin_map.h
+.\my_toggle_pinmux.o: ..\..\..\..\driverlib/rom_map.h
+.\my_toggle_pinmux.o: ..\..\..\..\driverlib/gpio.h
+.\my_toggle_pinmux.o: ..\..\..\..\inc/tm4c123gh6pm.h
